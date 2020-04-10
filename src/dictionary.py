3c8de3325211cd6c8453f465fee62454dc0e2624
Elijah Sawyers <elijahsawyers@gmail.com>
2020-04-10 02:00:30 -0500

    Update how the path of dictionary is found to be dynamic

--- src/dictionary.py
+++ src/dictionary.py
@@ -4,11 +4,13 @@ Emails: elijahsawyers@gmail.com
 Date: 03/28/2020
 '''
 
+import os
+
 def load_words():
     '''
     Returns a set of all words in the dictionary.
     '''
-    with open('words.txt') as word_file:
+    with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'words.txt')) as word_file:
         valid_words = set(word_file.read().split())
 
     return valid_words
