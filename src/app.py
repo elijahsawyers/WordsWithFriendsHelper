4ab347e61dd998a00453134b3a126e6be2a6fded
Elijah Sawyers <elijahsawyers@gmail.com>
2020-05-02 23:55:42 -0400

    Cleaning up python

--- src/app.py
+++ src/app.py
@@ -1,3 +1,9 @@
+'''
+Authors: Elijah Sawyers
+Emails: elijahsawyers@gmail.com
+Date: 03/28/2020
+'''
+
 from flask import (Flask, request, render_template, jsonify)
 
 import best_game_move
@@ -6,11 +12,20 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
+    '''
+    Main route, renders the application.
+    '''
+
     return render_template('index.html')
 
 @app.route('/bestGameMove', methods=['POST'])
 def compute_best_game_move():
+    '''
+    Given gameboard data, return the best possible game move.
+    '''
+
     return jsonify(best_game_move.compute(request.json))
 
+# Run the web app.
 if __name__ == '__main__':
     app.run()
--- src/dictionary.py
+++ src/dictionary.py
@@ -10,6 +10,7 @@ def load_words():
     '''
     Returns a set of all words in the dictionary.
     '''
+
     with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'words.txt')) as word_file:
         valid_words = set(word_file.read().split())
 
